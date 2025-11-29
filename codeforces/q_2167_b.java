db549dc6d690701cf61f8ce6fdf2591e5a1247d7
HARSH RAJ <harshraj250106@gmail.com>
2025-11-29 22:10:24 +0530

    29/11/2025

---
+++ codeforces/q_2167_b.java
@@ -0,0 +1,29 @@
+package codeforces;
+
+import java.util.*;
+
+public class q_2167_b {
+    public static String find(String s, String t) {
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        String s_sort = new String(arr);
+
+        char[] arr1 = t.toCharArray();
+        Arrays.sort(arr1);
+        String t_sort = new String(arr1);
+
+        if (!s_sort.equals(t_sort)) {
+            return "NO";
+        }
+
+        return "YES";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+
+        System.out.println(find(s, t));
+    }
+}
