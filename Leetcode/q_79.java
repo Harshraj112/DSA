011053d59a8c14855852a6df305f232d68be6fe3
HARSH RAJ <harshraj250106@gmail.com>
2025-12-23 21:20:24 +0530

    23/12/2025

--- Leetcode/q_79.java
+++ Leetcode/q_79.java
@@ -18,20 +18,14 @@ public class q_79 {
 
         if (index == word.length()) return true;
 
-        if (i < 0 || j < 0 || 
-            i >= board.length || j >= board[0].length || 
-            board[i][j] != word.charAt(index)) {
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index)) {
             return false;
         }
 
         char temp = board[i][j];
         board[i][j] = '#';
 
-        boolean found =
-                dfs(board, word, i + 1, j, index + 1) ||
-                dfs(board, word, i - 1, j, index + 1) ||
-                dfs(board, word, i, j + 1, index + 1) ||
-                dfs(board, word, i, j - 1, index + 1);
+        boolean found = dfs(board, word, i + 1, j, index + 1) || dfs(board, word, i - 1, j, index + 1) || dfs(board, word, i, j + 1, index + 1) || dfs(board, word, i, j - 1, index + 1);
 
         board[i][j] = temp;
 
