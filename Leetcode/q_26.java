ff9487f653ce12af349e7d14c22ca293f886e232
HARSH RAJ <harshraj250106@gmail.com>
2026-01-14 00:29:16 +0530

    14/1/26

--- Leetcode/q_26.java
+++ Leetcode/q_26.java
@@ -1,3 +1,16 @@
 public class q_26 {
-    
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int i = 0; // points to last unique element
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
 }
