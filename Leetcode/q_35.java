f71eb7ca5c7e4273750998083e77ac58f57d1e0b
HARSH RAJ <harshraj250106@gmail.com>
2026-01-14 10:33:59 +0530

    14/1/26

---
+++ Leetcode/q_35.java
@@ -0,0 +1,14 @@
+public class q_35 {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+    
+    public static void main(String[] args) {
+    }
+}
+
