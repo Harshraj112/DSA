579cdb09477f4ffd70e779516cc98e15de7ddd9f
HARSH RAJ <harshraj250106@gmail.com>
2026-01-15 18:59:05 +0530

    15/01/2026

---
+++ Leetcode/q_121.java
@@ -0,0 +1,15 @@
+public class q_121 {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i=1; i<prices.length; i++) {
+            int profit = prices[i] - minPrice;
+
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+}
