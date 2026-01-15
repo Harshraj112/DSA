bb36fa6f0d1723f59ceae86e1f465f58f0a04925
HARSH RAJ <harshraj250106@gmail.com>
2026-01-16 01:35:15 +0530

    16/01/2026

---
+++ Leetcode/q_136.java
@@ -0,0 +1,10 @@
+public class q_136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i : nums) {
+            result ^= i;
+        }
+
+        return result;
+    }
+}
