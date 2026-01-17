f144594a29a10e618434c6aa1a2bffbbe48150b9
HARSH RAJ <harshraj250106@gmail.com>
2026-01-17 16:45:09 +0530

    17/01/26

---
+++ Leetcode/q_1470.java
@@ -0,0 +1,13 @@
+public class q_1470 {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+        int idx = 0;
+
+        for(int i=0; i<n; i++) {
+            result[idx++] = nums[i];
+            result[idx++] = nums[n+i];
+        }
+
+        return result;
+    }
+}
