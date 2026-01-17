886ee096b752c00c08c68d79fdc3aa92083f88c1
HARSH RAJ <harshraj250106@gmail.com>
2026-01-17 15:19:27 +0530

    17/01/26

---
+++ Leetcode/q_1480.java
@@ -0,0 +1,12 @@
+public class q_1480 {
+    public int[] runningSum(int[] nums) {
+        for(int i=0; i<nums.length; i++) {
+            if(i == 0) {
+                nums[i] = nums[i];
+            } else {
+                nums[i] += nums[i-1];
+            }
+        }
+        return nums;
+    }
+}
