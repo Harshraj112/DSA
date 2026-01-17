1b53f8b79d031954bc93d1eb65c31304b4cb81a4
HARSH RAJ <harshraj250106@gmail.com>
2026-01-17 13:48:14 +0530

    17/01/26

---
+++ Leetcode/q_704.java
@@ -0,0 +1,19 @@
+public class q_704 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;  // To prevent potential overflow
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
