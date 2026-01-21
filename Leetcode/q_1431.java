03e5bd9b1b7dda0a7ecd737e7cafed79bd7a8670
HARSH RAJ <harshraj250106@gmail.com>
2026-01-21 23:29:06 +0530

    21 jan

---
+++ Leetcode/q_1431.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class q_1431 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+
+        int max = 0;
+        for (int c : candies) {
+            max = Math.max(max, c);
+        }
+
+        for (int c : candies) {
+            if (c + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+
+        return result;
+    }
+}
