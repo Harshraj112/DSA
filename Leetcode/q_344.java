fed16189d2c9a01b6e7a10f60d81ed30bcc5ca08
HARSH RAJ <harshraj250106@gmail.com>
2026-03-10 02:15:49 +0530

    10 march 2026

---
+++ Leetcode/q_344.java
@@ -0,0 +1,14 @@
+public class q_344 {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
