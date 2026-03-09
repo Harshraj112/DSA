7977c07ce100e22ca3954d9b67df0ea91d93702b
HARSH RAJ <harshraj250106@gmail.com>
2026-03-10 01:48:52 +0530

    10 march 2026

---
+++ Leetcode/q_1672.java
@@ -0,0 +1,16 @@
+public class q_1672 {
+    public int maximumWealth(int[][] accounts) {
+        int max = 0;
+        int curr = 0;
+
+        for(int i=0; i<accounts.length; i++) {
+            curr = 0;
+            for(int j=0; j<accounts[0].length; j++) {
+                curr += accounts[i][j];
+            }
+            max = Math.max(max, curr);
+        }
+
+        return max;
+    }
+}
