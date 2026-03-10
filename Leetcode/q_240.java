35971efe3e117f21683309d8f8a3c6dfd93d64fe
HARSH RAJ <harshraj250106@gmail.com>
2026-03-10 19:13:59 +0530

    10 mar

---
+++ Leetcode/q_240.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+public class q_240 {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+
+        if(words.length != pattern.length()) {
+            return false;
+        }
+
+        HashMap<Character, String> map1= new HashMap<>();
+        HashMap<String, Character> map2= new HashMap<>();
+
+        for(int i=0; i<pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if(!map1.containsKey(ch) && !map2.containsKey(word)) {
+                map1.put(ch, word);
+                map2.put(word, ch);
+            } else {
+                if (!word.equals(map1.get(ch)) || ch != map2.get(word)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
